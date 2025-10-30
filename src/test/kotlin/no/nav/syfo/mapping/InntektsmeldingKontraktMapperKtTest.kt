5f8f1ecd2de41f4a78956201d57fbc1bbe077943
Gustav Berggren <gustav.berggren@nav.no>
2025-10-30 10:27:12 +0100

    fjern nullsafe med dot generer fnr istedenfor

--- src/test/kotlin/no/nav/syfo/mapping/InntektsmeldingKontraktMapperKtTest.kt
+++ src/test/kotlin/no/nav/syfo/mapping/InntektsmeldingKontraktMapperKtTest.kt
@@ -1,5 +1,6 @@
 package no.nav.syfo.mapping
 
+import no.nav.helsearbeidsgiver.utils.test.wrapper.genererGyldig
 import no.nav.helsearbeidsgiver.utils.wrapper.Fnr
 import no.nav.inntektsmeldingkontrakt.Arbeidsgivertype
 import no.nav.syfo.FØRSTE_FEBRUAR
@@ -85,14 +86,14 @@ class InntektsmeldingKontraktMapperKtTest {
                 orgnummer = inntektsmeldingUtenFnr.arbeidsgiverOrgnummer,
                 arbeidsgiverPrivat = "arbeidsgiverPrivat",
                 aktorId = inntektsmeldingUtenFnr.aktorId.toString(),
-                fnr = Fnr("28014026691"),
+                fnr = Fnr.genererGyldig(),
                 data = om.writeValueAsString(inntektsmeldingUtenFnr),
             )
         val i = toInntektsmelding(dto, om)
         assertThat(i.journalpostId).isEqualTo("123")
         assertThat(i.sakId).isEqualTo("sakId")
         assertThat(i.arbeidsgiverOrgnummer).isEqualTo("1234")
-        assertThat(i.fnr).isEqualTo(dto.fnr?.verdi)
+        assertThat(i.fnr).isEqualTo(dto.fnr.verdi)
         assertThat(i.aktorId).isEqualTo("aktorId")
         assertThat(i.arbeidsgiverperioder.size).isEqualTo(1)
     }
