585453a146e1eafac03b30179a2eb050fce09bfd
Gustav Berggren <gustav.berggren@nav.no>
2025-10-30 16:46:09 +0100

    Forslag til leader election på bakgrunnsjobber og recuring jobs

---
+++ src/main/kotlin/no/nav/syfo/util/LeaderElectionManager.kt
@@ -0,0 +1,34 @@
+package no.nav.syfo.util
+
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.Serializable
+import no.nav.helsearbeidsgiver.utils.log.logger
+import no.nav.helsearbeidsgiver.utils.log.sikkerLogger
+import java.net.InetAddress
+
+object LeaderElectionManager {
+    val httpClient = createHttpClient(3)
+    val electorUrl = System.getProperty("ELECTOR_GET_URL")
+
+    fun isLeader() =
+        runBlocking {
+            try {
+                val electedPod: ElectedPod = httpClient.get(electorUrl).body()
+                val hostname = InetAddress.getLocalHost().hostName
+                hostname == electedPod.name
+            } catch (e: Exception) {
+                "Feil ved henting av leader status".also {
+                    logger().error(it)
+                    sikkerLogger().error(it, e)
+                }
+                false
+            }
+        }
+
+    @Serializable
+    data class ElectedPod(
+        val name: String,
+    )
+}
